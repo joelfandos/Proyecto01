7cc8f7aab917f05436141c3b7232783ceb46737d
Joel Fandos <100005820.joan23@fje.edu>
2019-11-06 17:14:05 +0100

    Añadido tabla incidencias a la bd

--- bd_proyecto01.sql
+++ bd_proyecto01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-10-2019 a las 16:58:06
+-- Tiempo de generación: 05-11-2019 a las 20:31:37
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -31,36 +31,37 @@ USE `bd_proyecto01`;
 --
 
 CREATE TABLE `recursos` (
-  `id_recurso` int(2) NOT NULL,
+  `id_recurso` int(11) NOT NULL,
   `recurso` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `tipo` int(1) NOT NULL,
   `descripcion` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `imagen` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+
 --
 -- Volcado de datos para la tabla `recursos`
 --
 
 INSERT INTO `recursos` (`id_recurso`, `recurso`, `tipo`, `descripcion`, `imagen`) VALUES
-(1, 'sala_multidisciplinaria', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
-(2, 'sala_multidisciplinaria', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
-(3, 'sala_multidisciplinaria', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
-(4, 'sala_multidisciplinaria', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
-(5, 'sala_info01', 1, 'Sala de ordenadores para hacer informatica o distracciones.', 'fotos/foto3.jpg'),
-(6, 'sala_info02', 1, 'Sala de ordenadores para hacer informatica o distracciones.', 'fotos/foto3.jpg'),
-(7, 'taller_cocina', 1, 'Sala dedicada a los talleres de cocina.', 'fotos/foto4.jpg'),
-(8, 'despacho01', 1, 'Despacho para utilizarlo en entrevistas.', 'fotos/foto5.jpg'),
-(9, 'despacho02', 1, 'Despacho para utilizarlo en entrevistas.', 'fotos/foto5.jpg'),
-(10, 'salon_actos', 1, 'Salón dedicado para hacer actos con un gran número de personas.', 'fotos/foto6.jpg'),
-(11, 'sala_reuniones', 1, 'Sala para cualquier tipo de reuniones.', 'fotos/foto7.jpg'),
-(12, 'proyector01', 2, 'QKK AK-80', 'fotos/foto8.jpg'),
-(13, 'proyector02', 2, 'QKK AK-80', 'fotos/foto8.jpg'),
-(14, 'portatil01', 2, 'HP OMEN 15-dc1023ns', 'fotos/foto9.jpg'),
-(15, 'portatil02', 2, 'HP OMEN 15-dc1023ns', 'fotos/foto9.jpg'),
-(16, 'portatil03', 2, 'HP OMEN 15-dc1023ns', 'fotos/foto9.jpg'),
-(17, 'movil01', 2, 'Iphone 11', 'fotos/foto10.jpg'),
-(18, 'movil02', 2, 'Iphone 11', 'fotos/foto10.jpg');
+(1, 'Sala Multidisciplinaria 1', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
+(2, 'Sala Multidisciplinaria 2', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
+(3, 'Sala Multidisciplinaria 3', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
+(4, 'Sala Multidisciplinaria 4', 1, 'Sala para hacer yoga, baile, presentaciones...', 'fotos/foto2.jpg'),
+(5, 'Sala de Informática 1', 1, 'Sala de ordenadores para hacer informatica o distracciones.', 'fotos/foto3.jpg'),
+(6, 'Sala de Informática 2', 1, 'Sala de ordenadores para hacer informatica o distracciones.', 'fotos/foto3.jpg'),
+(7, 'Taller de Cocina', 1, 'Sala dedicada a los talleres de cocina.', 'fotos/foto4.jpg'),
+(8, 'Despacho 1', 1, 'Despacho para utilizarlo en entrevistas.', 'fotos/foto5.jpg'),
+(9, 'Despacho 2', 1, 'Despacho para utilizarlo en entrevistas.', 'fotos/foto5.jpg'),
+(10, 'Salón de Actos', 1, 'Salón dedicado para hacer actos con un gran número de personas.', 'fotos/foto6.jpg'),
+(11, 'Sala de Reuniones', 1, 'Sala para cualquier tipo de reuniones.', 'fotos/foto7.jpg'),
+(12, 'Proyector 1', 2, 'QKK AK-80', 'fotos/foto8.jpg'),
+(13, 'Proyector 2', 2, 'QKK AK-80', 'fotos/foto8.jpg'),
+(14, 'Portátil 1', 2, 'HP OMEN 15-dc1023ns', 'fotos/foto9.jpg'),
+(15, 'Portátil 2', 2, 'HP OMEN 15-dc1023ns', 'fotos/foto9.jpg'),
+(16, 'Portátil 3', 2, 'HP OMEN 15-dc1023ns', 'fotos/foto9.jpg'),
+(17, 'Móvil 1', 2, 'Iphone 11', 'fotos/foto10.jpg'),
+(18, 'Móvil 2', 2, 'Iphone 11', 'fotos/foto10.jpg');
 
 -- --------------------------------------------------------
 
@@ -68,14 +69,19 @@ INSERT INTO `recursos` (`id_recurso`, `recurso`, `tipo`, `descripcion`, `imagen`
 -- Estructura de tabla para la tabla `reserva`
 --
 
+CREATE TABLE `incidencias` (
+  `id_incidencia` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `descripcion` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 CREATE TABLE `reserva` (
   `id_reserva` int(11) NOT NULL,
   `fecha_entrega` datetime NOT NULL,
-  `fecha_devolucion` datetime NOT NULL,
+  `fecha_devolucion` datetime DEFAULT NULL,
   `id_recurso_fk` int(11) NOT NULL,
   `usuario_fk` varchar(25) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 -- --------------------------------------------------------
 
 --
@@ -92,8 +98,8 @@ CREATE TABLE `tipos` (
 --
 
 INSERT INTO `tipos` (`id_tipo`, `tipo`) VALUES
-(1, 'espacios'),
-(2, 'electronica');
+(1, 'Inmuebles'),
+(2, 'Electrónica');
 
 -- --------------------------------------------------------
 
